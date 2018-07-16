3c215c11b440a771d880a0c2de3dcfa39d22ac91
RedstoneTim <39703953+RedstoneTim@users.noreply.github.com>
2018-07-16 15:08:03 +0200

    Fixed creative tabs (made by Mr. Random)
    Mr. Random tried to upload this fix but it didn't work, so we decided that I would upload it and give him credit.

--- src/main/java/pixeleyestudios/huntersdream/init/CreativeTabInit.java
+++ src/main/java/pixeleyestudios/huntersdream/init/CreativeTabInit.java
@@ -1,41 +1,25 @@
 package pixeleyestudios.huntersdream.init;
 
-import static pixeleyestudios.huntersdream.init.ItemInit.INGOT_PURE_SILVER;
-import static pixeleyestudios.huntersdream.init.ItemInit.SWORD_PURE_SILVER;
-
-import net.minecraft.block.Block;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+
+/**
+ * @author Mr. Random
+ */
 
 public class CreativeTabInit {
 
-	// CreativeTabs HUNTERSDREAM_MOBS = new Tab("Hunter's Dream Mobs", new
-	// ItemStack());
-	public static final CreativeTabs HUNTERSDREAM_TOOLS_AND_WEAPONS = new Tab("huntersDreamToolsCombat",
-			SWORD_PURE_SILVER);
-	public static final CreativeTabs HUNTERSDREAM_MISC = new Tab("huntersDreamMisc", INGOT_PURE_SILVER);
-
-	public static class Tab extends CreativeTabs {
-		public final ItemStack TAB_ICON;
-
-		public Tab(String name, ItemStack tabIcon) {
-			super(name);
-			this.TAB_ICON = tabIcon;
-		}
-
-		public Tab(String name, Item tabIcon) {
-			this(name, new ItemStack(tabIcon));
-		}
-
-		public Tab(String name, Block tabIcon) {
-			this(name, new ItemStack(tabIcon));
-		}
-
+	public static final CreativeTabs HUNTERSDREAM_TOOLS_AND_WEAPONS = new CreativeTabs("huntersDreamToolsCombat") {
 		@Override
 		public ItemStack getTabIconItem() {
-			return TAB_ICON;
+			return new ItemStack(ItemInit.SWORD_PURE_SILVER);
 		}
+	};
 
-	}
+	public static final CreativeTabs HUNTERSDREAM_MISC = new CreativeTabs("huntersDreamMisc") {
+		@Override
+		public ItemStack getTabIconItem() {
+			return new ItemStack(ItemInit.INGOT_PURE_SILVER);
+		}
+	};
 }
