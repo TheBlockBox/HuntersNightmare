f6f280006ed9838a86a7714169011339d764d594
RedstoneTim <39703953+RedstoneTim@users.noreply.github.com>
2020-01-26 10:39:39 +0100

    Remove version and mc_version in Reference.kt
    Currently not used for anything

--- src/main/kotlin/theblockbox/huntersdream/util/Reference.kt
+++ src/main/kotlin/theblockbox/huntersdream/util/Reference.kt
@@ -4,7 +4,5 @@ class Reference {
     companion object {
         const val MODID = "huntersdream"
         const val NAME = "Hunter's Dream"
-        const val VERSION = "2.0.0"
-        const val MC_VERSION = "1.14.2"
     }
 }
